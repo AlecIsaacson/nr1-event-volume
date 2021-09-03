6a1ff6782d7a5a5f9f15e3d0f9a88a36fcef8a97
aisaacson <aisaacson@newrelic.com>
2021-09-03 17:06:46 -0400

    Implement time picker

--- nerdlets/nr1-event-volume-nerdlet/index.js
+++ nerdlets/nr1-event-volume-nerdlet/index.js
@@ -1,7 +1,7 @@
 //Nerdlet to review all data in an account
 
 import React from 'react';
-import { NrqlQuery, Spinner, Table, TableHeader, TableHeaderCell, TableRow, TableRowCell, Stack, StackItem, JsonChart, HeadingText, BlockText, Grid, GridItem, LineChart } from 'nr1';
+import { PlatformStateContext, NrqlQuery, Spinner, Table, TableHeader, TableHeaderCell, TableRow, TableRowCell, Stack, StackItem, JsonChart, HeadingText, BlockText, Grid, GridItem, LineChart } from 'nr1';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
@@ -23,80 +23,90 @@ export default class EventVolumeNerdlet extends React.Component {
     console.debug("State: ", this.state)
     const { selectedEventType } = this.state;
     const eventTypeQuery = `SHOW eventTypes`
+    const eventTimeseriesQuery = `FROM \`${selectedEventType}\` SELECT bytecountestimate() TIMESERIES`
     return(
-      <Grid class-name="primary-grid" spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}>
-        <GridItem className="primary-content-container" columnSpan={12}>
-          <Stack fullWidth directionType={Stack.DIRECTION_TYPE.HORIZONTAL} gapType={Stack.GAP_TYPE.LOOSE}>
-            <StackItem grow={true} className="row-spacing">
-            <HeadingText style={{marginLeft: '25px'}}>Event Types in this Account</HeadingText>
-              <NrqlQuery accountId={this.accountId} query={eventTypeQuery}>
-                {({loading, error, data}) => {
-                  if (loading) return <Spinner />
-                  if (error) return <BlockText>{error.message}</BlockText>
-                  if (data) {
-                    console.debug('Raw Data:', data[0].data[0])
-                    // data[0].data[0].eventTypes.forEach((item, i) => {
-                    //   console.debug('Item:', item);
-                    // });
-                  }
-                  return(
-                    <Table items={data[0].data[0].eventTypes} className="top-chart">
-                      <TableHeader>
-                        <TableHeaderCell
-                          value={({ item }) => item}
-                          sortable
-                          sortingType={this.state.column_0}
-                          sortingOrder={1}
-                          onClick={this._onClickTableHeaderCell.bind(this, 'column_0')}
-                          width="fit-content"
-                        >
-                          Event Type
-                        </TableHeaderCell>
-                        <TableHeaderCell>
-                          Amount of Data (GB) 
-                        </TableHeaderCell>
-                      </TableHeader>
-                      {({ item }) => (
-                        <TableRow onClick={(evt, item, index) => {
-                          console.debug("Clicked:", item.item)
-                          this.setState({ selectedEventType: item.item })
-                        }}>
-                          <TableRowCell>
-                            {item}
-                          </TableRowCell>
-                          <TableRowCell alignmentType={TableRowCell.ALIGNMENT_TYPE.RIGHT}>
-                            <NrqlQuery accountId={this.accountId} query={`FROM \`${item}\` SELECT bytecountestimate()`}>
-                              {({ data, loading, error }) => {
-                                if (loading) return <Spinner />
-                                if (error) return <BlockText>{error.message}</BlockText>
-                                if (data) {
-                                  //console.debug('Volume data:', data[0].data[0].bytecountestimate)
-                                }
-                                var dataGb = data[0].data[0].bytecountestimate/10e8
-                                dataGb = dataGb.toFixed(2)
-                                return(dataGb)
-                              }}
-                            </NrqlQuery>
-                          </TableRowCell>  
-                        </TableRow>
-                      )
-                    }
-                    </Table>
-                  );
-                }}
-              </NrqlQuery> 
-            </StackItem>
-          </Stack>
-          { selectedEventType && <Stack fullWidth gapType={Stack.GAP_TYPE.LOOSE}>
-            {console.debug("Timeseries Event Type:", selectedEventType)}
-            <StackItem grow={true} className="row-spacing">
-              <HeadingText style={{marginLeft: '25px'}}>Event Volume Timeseries (bytes)</HeadingText>
-              <LineChart accountId={this.accountId} className="chart" query={`FROM \`${selectedEventType}\` SELECT bytecountestimate() TIMESERIES`}/>
-            </StackItem>
-          </Stack>
-          }
-        </GridItem>
-      </Grid>
+      <PlatformStateContext.Consumer>
+        {(platformUrlState) => {
+          console.debug(platformUrlState);
+          const { duration } = platformUrlState.timeRange;
+          const since = ` SINCE ${duration/60000} minutes ago`
+          return(
+            <Grid class-name="primary-grid" spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}>
+              <GridItem className="primary-content-container" columnSpan={12}>
+                <Stack fullWidth directionType={Stack.DIRECTION_TYPE.HORIZONTAL} gapType={Stack.GAP_TYPE.LOOSE}>
+                  <StackItem grow={true} className="row-spacing">
+                  <HeadingText style={{marginLeft: '25px'}}>Event Types in this Account</HeadingText>
+                    <NrqlQuery accountId={this.accountId} query={eventTypeQuery + since}>
+                      {({loading, error, data}) => {
+                        if (loading) return <Spinner />
+                        if (error) return <BlockText>{error.message}</BlockText>
+                        if (data) {
+                          console.debug('Raw Data:', data[0].data[0])
+                          // data[0].data[0].eventTypes.forEach((item, i) => {
+                          //   console.debug('Item:', item);
+                          // });
+                        }
+                        return(
+                          <Table items={data[0].data[0].eventTypes} className="top-chart">
+                            <TableHeader>
+                              <TableHeaderCell
+                                value={({ item }) => item}
+                                sortable
+                                sortingType={this.state.column_0}
+                                sortingOrder={1}
+                                onClick={this._onClickTableHeaderCell.bind(this, 'column_0')}
+                                width="fit-content"
+                              >
+                                Event Type
+                              </TableHeaderCell>
+                              <TableHeaderCell>
+                                Amount of Data (GB) 
+                              </TableHeaderCell>
+                            </TableHeader>
+                            {({ item }) => (
+                              <TableRow onClick={(evt, item, index) => {
+                                console.debug("Clicked:", item.item)
+                                this.setState({ selectedEventType: item.item })
+                              }}>
+                                <TableRowCell>
+                                  {item}
+                                </TableRowCell>
+                                <TableRowCell alignmentType={TableRowCell.ALIGNMENT_TYPE.RIGHT}>
+                                  <NrqlQuery accountId={this.accountId} query={`FROM \`${item}\` SELECT bytecountestimate() ` + since}>
+                                    {({ data, loading, error }) => {
+                                      if (loading) return <Spinner />
+                                      if (error) return <BlockText>{error.message}</BlockText>
+                                      if (data) {
+                                        //console.debug('Volume data:', data[0].data[0].bytecountestimate)
+                                      }
+                                      var dataGb = data[0].data[0].bytecountestimate/10e8
+                                      dataGb = dataGb.toFixed(2)
+                                      return(dataGb)
+                                    }}
+                                  </NrqlQuery>
+                                </TableRowCell>  
+                              </TableRow>
+                            )
+                          }
+                          </Table>
+                        );
+                      }}
+                    </NrqlQuery> 
+                  </StackItem>
+                </Stack>
+                { selectedEventType && <Stack fullWidth gapType={Stack.GAP_TYPE.LOOSE}>
+                  {console.debug("Timeseries Event Type:", selectedEventType)}
+                  <StackItem grow={true} className="row-spacing">
+                    <HeadingText style={{marginLeft: '25px'}}>{selectedEventType} Volume Timeseries (bytes)</HeadingText>
+                    <LineChart accountId={this.accountId} className="chart" query={eventTimeseriesQuery + since}/>
+                  </StackItem>
+                </Stack>
+                }
+              </GridItem>
+            </Grid>
+          );
+        }}
+      </PlatformStateContext.Consumer>
     );
   }
 }
