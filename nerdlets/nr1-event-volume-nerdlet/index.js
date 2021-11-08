5b4b156dc40fdc0633d18c1ca17d84db4f473e53
aisaacson <aisaacson@newrelic.com>
2021-11-08 09:14:33 -0500

    Update index.js

--- nerdlets/nr1-event-volume-nerdlet/index.js
+++ nerdlets/nr1-event-volume-nerdlet/index.js
@@ -1,7 +1,7 @@
 //Nerdlet to review all data in an account
 
 import React from 'react';
-import { PlatformStateContext, NrqlQuery, Spinner, Table, TableHeader, TableHeaderCell, TableRow, TableRowCell, Stack, StackItem, JsonChart, HeadingText, BlockText, Grid, GridItem, LineChart, NerdGraphQuery } from 'nr1';
+import { PlatformStateContext, NrqlQuery, Table, TableHeader, TableHeaderCell, TableRow, TableRowCell, Stack, StackItem, HeadingText, Grid, GridItem, LineChart } from 'nr1';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
@@ -45,13 +45,15 @@ export default class EventVolumeNerdlet extends React.Component {
             const eventVolumes = [];
             values.forEach((value, i) => {
               // console.debug("Promise", value) 
-              //console.debug("Data", eventTypes[i], value.data[0].data[0].bytecountestimate)
+              // console.debug("Data", eventTypes[i], value.data[0].data[0].bytecountestimate)
               eventVolumes.push({
                 eventType: eventTypes[i],
                 bytecountestimate: value.data[0].data[0].bytecountestimate
               });
             });
             this.setState({ eventVolumes })
+          }).catch(e => {
+            console.log(e)
           })
         });
       }
@@ -61,6 +63,11 @@ export default class EventVolumeNerdlet extends React.Component {
   _onClickTableHeaderCell(key, event, sortingData) {
     //console.debug("Header Cell", key, event, sortingData)
     //console.debug("Header Cell", state)
+    if (key === 'column_0') {
+      this.setState({ column_1: TableHeaderCell.SORTING_TYPE.NONE })
+    } else if (key === 'column_1') {
+      this.setState({ column_0: TableHeaderCell.SORTING_TYPE.NONE })
+    }
     this.setState({ [key]: sortingData.nextSortingType });
   }
   
@@ -71,53 +78,61 @@ export default class EventVolumeNerdlet extends React.Component {
     
     
     return (
-      <Grid class-name="primary-grid" spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}>
-        <GridItem className="primary-content-container" columnSpan={12}>
-          <Stack fullWidth directionType={Stack.DIRECTION_TYPE.HORIZONTAL} gapType={Stack.GAP_TYPE.LOOSE}>
-            <StackItem grow={true} className="row-spacing">
-              <Table items={eventVolumes} className="top-chart">
-                <TableHeader>
-                  <TableHeaderCell value={({ item }) => item.eventType}
-                    sortable
-                    sortingOrder={0}
-                    sortingType={this.state.column_0}
-                    onClick={this._onClickTableHeaderCell.bind(this, 'column_0')}
-                    width='3fr'
-                  >
-                    Event Type
-                  </TableHeaderCell>
-                  <TableHeaderCell value={({ item }) => item.bytecountestimate}
-                    sortable
-                    sortingOrder={1}
-                    sortingType={this.state.column_1}
-                    onClick={this._onClickTableHeaderCell.bind(this, 'column_1')}
-                  >
-                    Event Vol (GB) 
-                  </TableHeaderCell>
-                </TableHeader>
-                {({ item }) => (
-                  <TableRow
-                    onClick={(evt, item, index) => {
-                      console.debug("Clicked:", item)
-                      this.setState({ selectedEventType: item.item.eventType})
-                    }}
-                  >
-                    <TableRowCell>{item.eventType}</TableRowCell>
-                    <TableRowCell>{(item.bytecountestimate / 10e8).toFixed(2)}</TableRowCell>
-                  </TableRow>
-                )}
-              </Table>
-            </StackItem>
-            { selectedEventType && <Stack fullWidth gapType={Stack.GAP_TYPE.LOOSE}>
-               <StackItem grow={true} className="row-spacing">
-                 <HeadingText style={{marginLeft: '25px'}}>{selectedEventType} Volume Timeseries (bytes)</HeadingText>
-                 <LineChart accountId={accountId} className="chart" query={`FROM \`${selectedEventType}\` SELECT bytecountestimate() TIMESERIES`}/>
-               </StackItem>
-             </Stack>
-            }
-          </Stack>
-        </GridItem>
-      </Grid>
+      <PlatformStateContext.Consumer>
+        {(platformUrlState) => {
+          const { duration } = platformUrlState.timeRange;
+          const since = ` SINCE ${duration/60000} minutes ago`
+          return(
+            <Grid class-name="primary-grid" spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}>
+              <GridItem className="primary-content-container" columnSpan={12}>
+                <Stack fullWidth directionType={Stack.DIRECTION_TYPE.HORIZONTAL} gapType={Stack.GAP_TYPE.LOOSE}>
+                  <StackItem grow={true} className="row-spacing">
+                    <Table items={eventVolumes} className="top-chart">
+                      <TableHeader>
+                        <TableHeaderCell value={({ item }) => item.eventType}
+                          sortable
+                          //sortingOrder={0}
+                          sortingType={this.state.column_0}
+                          onClick={this._onClickTableHeaderCell.bind(this, 'column_0')}
+                          width='3fr'
+                        >
+                          Event Type
+                        </TableHeaderCell>
+                        <TableHeaderCell value={({ item }) => item.bytecountestimate}
+                          sortable
+                          //sortingOrder={1}
+                          sortingType={this.state.column_1}
+                          onClick={this._onClickTableHeaderCell.bind(this, 'column_1')}
+                        >
+                          Event Vol (GB) 
+                        </TableHeaderCell>
+                      </TableHeader>
+                      {({ item }) => (
+                        <TableRow
+                          onClick={(evt, item, index) => {
+                            //console.debug("Clicked:", item)
+                            this.setState({ selectedEventType: item.item.eventType})
+                          }}
+                        >
+                          <TableRowCell>{item.eventType}</TableRowCell>
+                          <TableRowCell>{(item.bytecountestimate / 10e8).toFixed(2)}</TableRowCell>
+                        </TableRow>
+                      )}
+                    </Table>
+                  </StackItem>
+                  { selectedEventType && <Stack fullWidth gapType={Stack.GAP_TYPE.LOOSE}>
+                     <StackItem grow={true} className="row-spacing">
+                       <HeadingText style={{marginLeft: '25px'}}>{selectedEventType} Volume Timeseries (bytes)</HeadingText>
+                       <LineChart accountId={accountId} className="chart" query={`FROM \`${selectedEventType}\` SELECT bytecountestimate() TIMESERIES` + since}/>
+                     </StackItem>
+                   </Stack>
+                  }
+                </Stack>
+              </GridItem>
+            </Grid>
+          );
+        }}
+      </PlatformStateContext.Consumer>
     );
   }
 }
